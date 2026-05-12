9a2916296e09391fc4cb309394fce63b11619f08
ChiragBangera <chiragbangera468@gmail.com>
2026-05-13 00:46:41 +0530

    blinky blinky

--- example/target/thumbv7em-none-eabihf/debug/deps/meet_my_software-6e17e81385ba68e7.d
+++
@@ -1,5 +0,0 @@
-/Users/chiragbangera/Developer/micro_bit/example/target/thumbv7em-none-eabihf/debug/deps/meet_my_software-6e17e81385ba68e7.d: src/main.rs
-
-/Users/chiragbangera/Developer/micro_bit/example/target/thumbv7em-none-eabihf/debug/deps/libmeet_my_software-6e17e81385ba68e7.rmeta: src/main.rs
-
-src/main.rs:
